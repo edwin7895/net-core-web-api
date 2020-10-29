da1687150b3ee5948de0a9760b7e8cb04c8594d6
miguelmejiaq@outlook.com <miguelmejiaq@outlook.com>
2020-10-28 22:25:29 -0500

    Added fully functionalities to MusicController

--- webApi.Api/Controllers/MusicController.cs
+++ webApi.Api/Controllers/MusicController.cs
@@ -27,6 +27,14 @@ namespace webApi.Api.Controllers
             return Ok(musicResources);
         }
 
+        [HttpGet("{id}")]
+        public async Task<ActionResult<MusicResource>> GetMusicById(int id)
+        {
+            var music = await _musicService.GetMusicById(id);
+            var musicResource = _mapper.Map<Music,MusicResource>(music);
+            return Ok(musicResource);
+        }
+
         [HttpPost]
         public async Task<ActionResult<MusicResource>> CreateMusic([FromBody] SaveMusicResource saveMusicResource)
         {
@@ -35,6 +43,31 @@ namespace webApi.Api.Controllers
             var music = await _musicService.GetMusicById(newMusic.Id);
             var musicResource = _mapper.Map<Music, MusicResource>(music);
             return Ok(musicResource);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult<MusicResource>> UpdateMusic(int id, [FromBody] SaveMusicResource saveMusicResource)
+        {
+            var musicToBeUpdate = await _musicService.GetMusicById(id);
+            if (musicToBeUpdate == null)
+                return NotFound();
+            var music = _mapper.Map<SaveMusicResource, Music>(saveMusicResource);
+            await _musicService.UpdateMusic(musicToBeUpdate, music);
+            var updatedMusic = await _musicService.GetMusicById(id);
+            var updatedMusicResource = _mapper.Map<Music, MusicResource>(updatedMusic);
+            return Ok(updatedMusicResource);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteMusic(int id)
+        {
+            if (id == 0)
+                return BadRequest();
+            var music = await _musicService.GetMusicById(id);
+            if (music == null)
+                return NotFound();
+            await _musicService.DeleteMusic(music);
+            return NoContent();
         }
     } 
 }
