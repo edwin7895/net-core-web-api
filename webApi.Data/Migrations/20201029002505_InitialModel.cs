0e97cac87d4ca10a7a47b521ee9ec4c72bd069c7
miguelmejiaq@outlook.com <miguelmejiaq@outlook.com>
2020-10-28 19:31:02 -0500

    add .gitignore

---
+++ webApi.Data/Migrations/20201029002505_InitialModel.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace webApi.Data.Migrations
+{
+    public partial class InitialModel : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "artists",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(maxLength: 50, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_artists", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "musics",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(maxLength: 50, nullable: false),
+                    ArtistId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_musics", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_musics_artists_ArtistId",
+                        column: x => x.ArtistId,
+                        principalTable: "artists",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_musics_ArtistId",
+                table: "musics",
+                column: "ArtistId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "musics");
+
+            migrationBuilder.DropTable(
+                name: "artists");
+        }
+    }
+}
